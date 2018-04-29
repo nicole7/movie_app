5cae19940dec171e5008638c1f3c66bacfc34863
Nicole-Gasperini <nicole.gasperini88@gmail.com>
2018-04-29 13:17:54 -0600

    Fix view folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :movies
+  resources :users
+
+  resources :orders do
+    resources :movies
+  end
+
+  root "movies#index"
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
+
 end
